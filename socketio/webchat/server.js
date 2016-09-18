045865d403a087492fd278556dff1cebe2df0306
Shi Guohuang <sgh1982@gmai.com>
2016-09-18 20:36:01 +0800

    socket.io & angular

--- socketio/webchat/server.js
+++ socketio/webchat/server.js
@@ -10,55 +10,59 @@ var users = [];
 app.use(express.static(path.join(__dirname, "public")));
 
 io.on('connection', function(socket) {
-	console.log('new connection made');
+  console.log('new connection made');
 
-	// Join private room
-	socket.on('join-private', function(data) {
-		socket.join('private');
-		console.log(data.nickname + ' joined private');
-	});
+  // Join private room
+  socket.on('join-private', function(data) {
+    socket.join('private');
+    console.log(data.nickname + ' joined private');
+  });
 
-	socket.on('private-chat', function(data) {
-		socket.broadcast.to('private').emit('show-message', data.message);
-	});
+  socket.on('private-chat', function(data) {
+    socket.broadcast.to('private').emit('show-message', data.message);
+  });
 
-	// Show all users when first logged in
-	socket.on('get-users', function(data) {
-		socket.emit('all-users', users);
-	});
+  // Show all users when first logged on
+  socket.on('get-users', function(data) {
+    socket.emit('all-users', users);
+  });
 
-	// When new socket joins
-	socket.on('join', function(data) {
-		socket.nickname = data.nickname;
-		var userObj = {
-			nickname: data.nickname,
-			socketid: socket.id
-		}
+  // When new socket joins
+  socket.on('join', function(data) {
+    socket.nickname = data.nickname;
+    // users[socket.nickname] = socket; 
+    var userObj = {
+      nickname: data.nickname,
+      socketid: socket.id
+    }
+    users.push(userObj);
+    console.log(users);
+    io.emit('all-users', users);
+  });
 
-		users.push(userObj);
-		console.log(users);
-		io.emit('all-users', users);
-	});
+  // Send a message
+  socket.on('send-message', function(data) {
+    // socket.broadcast.emit('message-received', data);
+    io.emit('message-received', data);
+  });
 
-	// Send a message
-	socket.on('send-message', function(data) {
-		io.emit('message-received', data);
-	});
+  // Send a 'like' to the user of your choice
+  socket.on('send-like', function(data) {
+    console.log(data);
+    console.log(data.like);
+    console.log(data.from);
+    socket.broadcast.to(data.like).emit('user-liked', data);
+  });
 
-	// Send a like to the user of your choice
-	socket.on('send-like', function(data) {
-		console.log(data);
-		socket.broadcast.to(data.like).emit('user-liked', data);
-	});
+  socket.on('disconnect', function() {
+    users = users.filter(function(item) {
+      return item.nickname !== socket.nickname;
+    });
+    io.emit('all-users', users);
+  });
 
-	socket.on('disconnect', function(data) {
-		users = users.filter(function(item) {
-			return item.nickname !== socket.nickname;
-		});
-		io.emit('all-users', users);
-	});
 });
 
 server.listen(port, function() {
-	console.log("Listening on port " + port);
+  console.log("Listening on port " + port);
 });
